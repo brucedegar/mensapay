9d58f695b23e7bd1ff4f84cba42be8d86564dc2b
Bruce <bruce.degar@gmail.com>
2025-01-07 15:13:41 +1100

    Compress files

--- ui-dashboard/vite.config.ts
+++ ui-dashboard/vite.config.ts
@@ -1,20 +1,30 @@
 import path from "path";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dynamicImport from "vite-plugin-dynamic-import";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import svgr from "vite-plugin-svgr";
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         sourcemap: false,
-        minify: "esbuild"
+        minify: "esbuild",
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'], // Split vendor libraries
+                },
+            },
+        },
+        chunkSizeWarningLimit: 2000, // Adjust the warning limit if necessary
     },
     resolve: {
         alias: {
             src: path.resolve(__dirname, "/src")
         }
     },
-    plugins: [basicSsl(), svgr(), dynamicImport(), react()]
+    plugins: [basicSsl(), svgr(), dynamicImport(), react(), compression()
+    ]
 });
--- ui-payment/vite.config.ts
+++ ui-payment/vite.config.ts
@@ -1,17 +1,28 @@
 import path from "path";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import dynamicImport from "vite-plugin-dynamic-import";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'], // Split vendor libraries
+                },
+            },
+        },
+        chunkSizeWarningLimit: 2000, // Adjust the warning limit if necessary
+    },
     resolve: {
         alias: {
             src: path.resolve(__dirname, "/src")
         }
     },
     // @ts-ignore
-    plugins: [basicSsl(), svgr(), dynamicImport(), react()]
+    plugins: [basicSsl(), svgr(), dynamicImport(), react(), compression()]
 });
